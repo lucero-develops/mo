217c5ba93752e667940a7786274844a680b8a018
lucero-develops <mike.j.lucero@gmail.com>
2017-01-28 12:16:19 -0500

    my attempt

--- main.js
+++ main.js
@@ -2,23 +2,31 @@
  * Programming Quiz: Murder Mystery (3-4)
  */
 
-// change the value of `room` and `suspect` to test your code
+// change the value of `room` and `suspect`
+//
+// to test your code
 var room = "dining room";
 var suspect = "Mr. Parkes";
 
 var weapon = "";
 var solved = false;
 
-if (/* your conditional goes here */) {
-    
-} else if (/* your conditional goes here */) {
-    
-} else if (/* your conditional goes here */) {
-    
-} else {
-    
+if (room ==="dining room"&& suspect==="Mr. Parkes") {
+	weapon = "knife";
+	solved = true;   
+} else if(room ==="billiards room"&& suspect==="Mrs. Sparr") {
+	weapon = "pool stick";
+	solved = true;
+}else if (room ==="gallery"&& suspect==="Ms. Van Cleve") {
+	weapon = "trophy";
+	solved = true;
+}else if (room ==="ballroom"&& suspect==="Mr. Kalehoff") {
+	weapon ="poison";
+	solved = true;
+}else {
+	solved = false;
 }
 
 if (solved) {
-	console.log(/* your message goes here*/);
+	console.log(suspect+" did it in the "+room+" with the "+weapon +"!");
 }
